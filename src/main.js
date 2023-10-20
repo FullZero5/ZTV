9915f211c614b85ab18141311fc8d6abe8e40fe8
Fullzero <fullzero5@gmail.com>
2023-10-20 04:10:41 +0300

    add analytics

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
-import { createApp, useAttrs } from 'vue'
+import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import 'flowbite'
 import { createPinia } from "pinia"
+import { inject } from '@vercel/analytics'
+ 
+inject()
 
 const pinia = createPinia()
 
